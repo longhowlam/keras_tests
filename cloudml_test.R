f2cb165311c37e433ee576901612e28a53459fa0
longhowlam <longhowlam@gmail.com>
2018-04-07 22:40:15 +0200

    cc

--- cloudml_test.R
+++ cloudml_test.R
@@ -1,10 +1,19 @@
 #devtools::install_github("rstudio/cloudml")
 library(cloudml)
+library(keras)
 #gcloud_install()
 gcloud_init()
 
-cloudml_train(file="mnist_mlp.R", master_type = "large_model")
+job = cloudml_train(file="mnist_mlp.R", master_type = "large_model")
+
 job_status()
-job_collect()
+model_out = job_collect()
+
 job_list()
 job_cancel()
+
+
+model_out$
+
+
+export_savedmodel()
